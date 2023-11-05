e63b3e5f11f1b4fc85dd68661fa4cc09cf576622
Andres Arenas <aarenas@livevox.com>
2023-11-05 13:21:59 -0500

    feat(base): adding prettier and husky

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+/**
+ * https://prettier.io/docs/en/options.html
+ */
+module.exports = {
+	arrowParens: 'always',
+	bracketSpacing: true,
+	bracketSameLine: false,
+	jsxSingleQuote: true,
+	printWidth: 80,
+	quoteProps: 'as-needed',
+	semi: true,
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'es5',
+	useTabs: true,
+};
